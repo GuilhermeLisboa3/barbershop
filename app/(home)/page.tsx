3f174fc82b45d4524ffad675e38b7cc142477daf
Guilherme Lisboa <guime0162@gmail.com>
2024-01-30 14:40:15 -0300

    feat: add welcome message

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,21 @@
+import { format } from "date-fns";
 import Header from "../_components/header";
+import { ptBR } from "date-fns/locale";
 
 export default function Home() {
   return (
     <div>
       <Header/>
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Guilherme!</h2>
+        <p className="capitalize text-sm">
+          {
+            format(new Date(), "EEEE',' d 'de' MMMM ", {
+              locale: ptBR
+            })
+          }
+        </p>
+      </div>
     </div>
   );
 }
